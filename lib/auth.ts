da55f31bf1b42eecf1f454499e295bc1156ac2a6
Kartik <kartik200421@gmail.com>
2025-08-13 02:14:56 +0530

    removed rate limit(custom)

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,39 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, LiteralUnion, Models } from "better-auth";
 
 export const auth = betterAuth({
-  rateLimit: {
-    enabled: true,
-    window: 10,
-    max: 100,
-    customRules: {
-      "/": {
-        window: 100,
-        max: 1,
+  advanced: {
+    ipAddress: {
+      ipAddressHeaders: ["x-client-ip", "x-forwarded-for"],
+      disableIpTracking: false,
+    },
+    useSecureCookies: true,
+    disableCSRFCheck: false,
+    crossSubDomainCookies: {
+      enabled: true,
+      additionalCookies: ["custom_cookie"],
+      domain: "example.com",
+    },
+    cookies: {
+      session_token: {
+        name: "custom_session_token",
+        attributes: {
+          httpOnly: true,
+          secure: true,
+        },
       },
     },
-    storage: "memory",
-    modelName: "rateLimit",
+    defaultCookieAttributes: {
+      httpOnly: true,
+      secure: true,
+    },
+    cookiePrefix: "myapp",
+    database: {
+      // If your DB is using auto-incrementing IDs, set this to true.
+      useNumberId: false,
+      generateId: (options: { model: LiteralUnion<Models, string>; size?: number }) => {
+        return "my-super-unique-id";
+      },
+      defaultFindManyLimit: 1,
+    },
   },
 });
